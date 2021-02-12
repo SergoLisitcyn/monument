296874e25582eb389737ccb21a29ed50fd751067
sergo <sergolisitcyn@gmail.com>
2021-02-12 18:19:47 +0200

    fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -190,7 +190,7 @@ $this->title = 'Памятники на могилу в Калининграде
                         </div>
                         <div class="col-xl-6 col-md-6">
                             <div class="business_thumb">
-                                <img src="../img/main/granit-min.jpg" alt="Памятники на могилу в Калининграде">
+                                <img src="../img/main/granit.jpg" alt="Памятники на могилу в Калининграде">
                             </div>
                         </div>
                     </div>
@@ -210,7 +210,7 @@ $this->title = 'Памятники на могилу в Калининграде
                         </div>
                         <div class="col-xl-6 col-md-6">
                             <div class="business_thumb">
-                                <img src="../img/main/granit-min.jpg" alt="Памятники на могилу Калининград">
+                                <img src="../img/main/granit.jpg" alt="Памятники на могилу Калининград">
                             </div>
                         </div>
                     </div>
@@ -229,7 +229,7 @@ $this->title = 'Памятники на могилу в Калининграде
                         </div>
                         <div class="col-xl-6 col-md-6">
                             <div class="business_thumb">
-                                <img src="../img/main/granit-min.jpg" alt="Памятники на могилу в Калининграде">
+                                <img src="../img/main/granit.jpg" alt="Памятники на могилу в Калининграде">
                             </div>
                         </div>
                     </div>
